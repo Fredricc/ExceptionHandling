12f360837b4c42e8fb1b7c709fa915caff9582f2
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-12-28 17:45:41 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //using (var streamReader = new StreamReader(@"c:\file.zip"))
+                //{
+                //    var content = streamReader.ReadToEnd();
+                //}
+                var api = new YouTubeApi();
+                var videos = api.GetVideos("mosh");
+                
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Sorry, unexpected error occured.");
+                Console.WriteLine(ex.Message);
+                
+                Console.ReadLine();
+            }
+            
+            
+        }
+    }
+}
